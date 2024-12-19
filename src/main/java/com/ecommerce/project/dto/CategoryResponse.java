12c66f850a9282cbc42b675b1d82e9b321cb4725
kubhandare <kunalbhandare422@gmail.com>
2024-12-19 13:18:22 +0530

    Pagination - Added pagination metadata in response

--- src/main/java/com/ecommerce/project/dto/CategoryResponse.java
+++ src/main/java/com/ecommerce/project/dto/CategoryResponse.java
@@ -10,5 +10,10 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryResponse {
-    List<CategoryDTO> content;
+    private List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private Boolean lastPage;
 }
