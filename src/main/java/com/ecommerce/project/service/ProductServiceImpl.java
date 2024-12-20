968d22c2cdf2dce5c2cfa80f573dfaa7444f56cc
kubhandare <kunalbhandare422@gmail.com>
2024-12-20 09:27:14 +0530

    Product Module - Implementation of POST Endpoint (Adding Product)

---
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.dto.ProductDTO;
+import com.ecommerce.project.exception.ResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.repository.CategoryRepository;
+import com.ecommerce.project.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        product.setCategory(category);
+        product.setImage("default.png");
+
+        double specialPrice = product.getPrice() - ((product.getDiscount() / 100) * product.getPrice());
+        product.setSpecialPrice(specialPrice);
+
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
+}
