17bd26120268419ed6321a3357c76052880a44dc
kubhandare <kunalbhandare422@gmail.com>
2024-12-24 18:00:50 +0530

    Spring Security - Implemented JWT Security

--- src/main/java/com/ecommerce/project/repository/UserRepository.java
+++ src/main/java/com/ecommerce/project/repository/UserRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<Object> findByUserName(String username);
+    Optional<User> findByUserName(String username);
+
+    Boolean existsByUserName(String username);
+
+    Boolean existsByEmail(String email);
 }
