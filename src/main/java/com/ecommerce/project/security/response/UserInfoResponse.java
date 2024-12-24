ee28c7825bd68ee42fd03801e3ea0078bb4c9954
kubhandare <kunalbhandare422@gmail.com>
2024-12-24 18:27:53 +0530

    Spring Security - Cookie Based Authentication and Implementation of getUserDetails endpoint

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -15,6 +15,7 @@ public class UserInfoResponse {
     private String username;
     private List<String> roles;
 
+
     public UserInfoResponse(Long id, String username, List<String> roles, String jwtToken) {
         this.id = id;
         this.username = username;
@@ -22,4 +23,9 @@ public class UserInfoResponse {
         this.jwtToken = jwtToken;
     }
 
+    public UserInfoResponse(Long id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
 }
